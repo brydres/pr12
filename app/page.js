8124c5c457f07f87a152c799f0150f76e2688660
brydres <bryanb1489@gmail.com>
2023-07-28 08:20:26 -0500

    keep going

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
 import Image from 'next/image'
-import header from './components/Header'
+import Header from './components/Header'
+import Container from './components/Container'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import "@/app/globals.css"
 
 export default function Home() {
   return (
     <>
     <Header />
+    <Container />
+    <Main />
+    <Footer />
 
     </>
   )
